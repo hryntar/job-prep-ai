a55468f8d913bef1ac1cf94f7b69223518a83478
hryntar <hrynchuktt@gmail.com>
2025-08-26 18:54:57 +0300

    env setup

---
+++ src/data/env/server.ts
@@ -0,0 +1,10 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import z from "zod";
+
+export const env = createEnv({
+   server: {
+      CLERK_SECRET_KEY: z.string().min(1),
+   },
+   emptyStringAsUndefined: true,
+   experimental__runtimeEnv: process.env,
+})
