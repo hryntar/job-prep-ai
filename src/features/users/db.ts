f574891b8437ed7d9c2e86301230833d4ad1cf95
hryntar <hrynchuktt@gmail.com>
2025-09-02 17:37:21 +0300

    clerk webhooks

---
+++ src/features/users/db.ts
@@ -0,0 +1,15 @@
+import { db } from "@/drizzle/db";
+import { UsersTable } from "@/drizzle/schema";
+import { eq } from "drizzle-orm";
+
+export async function upsertUser(user: typeof UsersTable.$inferInsert) {
+   await db.insert(UsersTable).values(user).onConflictDoUpdate({
+      target: [UsersTable.id],
+      set: user
+   })
+}
+
+export async function deleteUser(id: string) {
+   await db.delete(UsersTable).where(eq(UsersTable.id, id));
+
+}
---
+++ src/features/users/dbCache.ts
@@ -0,0 +1,16 @@
+import { getGlobalTag, getIdTag } from "@/lib/dataCache"
+import { get } from "http";
+import { revalidateTag } from "next/cache";
+
+export function getUserGlobalTag() {
+   return getGlobalTag("users");
+}
+
+export function getUserIdTag(id: string) {
+   return getIdTag("users", id);
+}
+
+export function revalidateUserCache(id: string) {
+   revalidateTag(getUserGlobalTag());
+   revalidateTag(getUserIdTag(id));
+}
