daff213768092ec8297c8bc210694c6549002fde
hryntar <hrynchuktt@gmail.com>
2025-09-06 18:19:26 +0300

    New job info page implementation

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,12 +1,16 @@
-import { Card, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { db } from "@/drizzle/db";
 import { JobInfoTable } from "@/drizzle/schema";
 import { JobInfoForm } from "@/features/jobInfos/components/JobInfoForm";
 import { getJobInfoUserTag } from "@/features/jobInfos/dbCache";
+import { formatExperienceLevel } from "@/features/jobInfos/lib/formatters";
 import { getCurrentUser } from "@/services/clerk/lib/getCurrentUser";
 import { desc, eq } from "drizzle-orm";
-import { Loader2Icon } from "lucide-react";
+import { ArrowRightIcon, Loader2Icon, PlusIcon } from "lucide-react";
 import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default function AppPage() {
@@ -32,6 +36,51 @@ async function JobInfos() {
    if (jobInfos.length === 0) {
       return <NoJobInfos />;
    }
+
+   return (
+      <div className="container my-4 ">
+         <div className="flex gap-2 justify-between mb-6">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl">Select a job description</h1>
+            <Button asChild>
+               <Link href="/app/job-infos/new">
+                  <PlusIcon />
+                  Create Job Description
+               </Link>
+            </Button>
+         </div>
+         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 has-hover:*:not-hover:opacity-70">
+            {jobInfos.map((jobInfo) => (
+               <Link className="hover:scale-[1.02] transition-[transform_opacity]" key={jobInfo.id} href={`/app/job-infos/${jobInfo.id}`}>
+                  <Card className="h-full">
+                     <div className="flex items-center justify-between h-full">
+                        <div className="space-y-4 h-full">
+                           <CardHeader>
+                              <CardTitle className="text-lg">{jobInfo.name}</CardTitle>
+                           </CardHeader>
+                           <CardContent className="text-muted-foreground line-clamp-3">{jobInfo.description}</CardContent>
+                           <CardFooter className="flex gap-2">
+                              <Badge variant="outline">{formatExperienceLevel(jobInfo.experienceLevel)}</Badge>
+                              {jobInfo.title && <Badge variant="outline">{jobInfo.title}</Badge>}
+                           </CardFooter>
+                        </div>
+                        <CardContent>
+                           <ArrowRightIcon className="size-6" />
+                        </CardContent>
+                     </div>
+                  </Card>
+               </Link>
+            ))}
+            <Link className="transition-opacity" href="/app/job-infos/new">
+               <Card className="h-full flex items-center justify-center border-dashed border-3 bg-transparent hover:border-primary/50 transition-colors shadow-none">
+                  <div className="flex flex-col items-center justify-center h-full text-muted-foreground">
+                     <PlusIcon className="size-8 mb-2" />
+                     <p>Create Job Description</p>
+                  </div>
+               </Card>
+            </Link>
+         </div>
+      </div>
+   );
 }
 
 function NoJobInfos() {
---
+++ src/components/BackLink.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { ArrowLeftIcon } from "lucide-react";
+
+export function BackLink({ href, children, className = "" }: { href: string; children: React.ReactNode; className?: string }) {
+   return (
+      <Button asChild variant="ghost" size="sm" className={cn("-ml-3", className)}>
+         <Link href={href} className="flex gap-2 items-center text-sm text-muted-foreground">
+            <ArrowLeftIcon />
+            {children}
+         </Link>
+      </Button>
+   );
+}
