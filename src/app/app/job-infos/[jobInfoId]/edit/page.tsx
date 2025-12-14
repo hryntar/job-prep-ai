db13ed5903b0ded446242a372737bcfed0e59e50
hryntar <hrynchuktt@gmail.com>
2025-12-14 16:25:07 +0200

    edit interview page

--- src/app/app/job-infos/[jobInfoId]/edit/page.tsx
+++ src/app/app/job-infos/[jobInfoId]/edit/page.tsx
@@ -12,44 +12,44 @@ import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
 export default async function NewJobInfoPage({ params }: { params: Promise<{ jobInfoId: string }> }) {
-   const { jobInfoId } = await params;
+  const { jobInfoId } = await params;
 
-   const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
-   if (!uuidRegex.test(jobInfoId)) {
-      return notFound();
-   }
+  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+  if (!uuidRegex.test(jobInfoId)) {
+    return notFound();
+  }
 
-   return (
-      <div className="container my-4 max-w-5xl space-y-4">
-         <JobInfoBackLink jobInfoId={jobInfoId} />
-         <h1 className="text-3xl md:text-4xl">Edit Job Description</h1>
+  return (
+    <div className="container my-4 max-w-5xl space-y-4">
+      <JobInfoBackLink jobInfoId={jobInfoId} />
+      <h1 className="text-3xl md:text-4xl">Edit Job Description</h1>
 
-         <Card>
-            <CardContent>
-               <Suspense fallback={<Loader2Icon className="size-24 animate-spin mx-auto" />}>
-                  <SuspendedForm jobInfoId={jobInfoId} />
-               </Suspense>
-            </CardContent>
-         </Card>
-      </div>
-   );
+      <Card>
+        <CardContent>
+          <Suspense fallback={<Loader2Icon className="size-24 animate-spin mx-auto" />}>
+            <SuspendedForm jobInfoId={jobInfoId} />
+          </Suspense>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 async function SuspendedForm({ jobInfoId }: { jobInfoId: string }) {
-   const { userId, redirectToSignIn } = await getCurrentUser();
-   if (userId == null) return redirectToSignIn();
+  const { userId, redirectToSignIn } = await getCurrentUser();
+  if (userId == null) return redirectToSignIn();
 
-   const jobInfo = await getJobInfo(jobInfoId, userId);
-   if (jobInfo == null) return notFound();
+  const jobInfo = await getJobInfo(jobInfoId, userId);
+  if (jobInfo == null) return notFound();
 
-   return <JobInfoForm jobInfo={jobInfo} />;
+  return <JobInfoForm jobInfo={jobInfo} />;
 }
 
 async function getJobInfo(jobInfoId: string, userId: string) {
-   "use cache";
-   cacheTag(getJobInfoIdTag(jobInfoId));
+  "use cache";
+  cacheTag(getJobInfoIdTag(jobInfoId));
 
-   return db.query.JobInfoTable.findFirst({
-      where: and(eq(JobInfoTable.id, jobInfoId), eq(JobInfoTable.userId, userId)),
-   });
+  return db.query.JobInfoTable.findFirst({
+    where: and(eq(JobInfoTable.id, jobInfoId), eq(JobInfoTable.userId, userId)),
+  });
 }
--- src/app/app/job-infos/[jobInfoId]/page.tsx
+++ src/app/app/job-infos/[jobInfoId]/page.tsx
@@ -15,98 +15,94 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 
 const options = [
-   {
-      label: "Answer Technical Questions",
-      description: "Challenge yourself with practice questions tailored to your job description.",
-      href: "questions",
-   },
-   {
-      label: "Practice Interviewing",
-      description: "Simulate a real interview with AI-powered mock interviews.",
-      href: "interviews",
-   },
-   {
-      label: "Refine Your Resume",
-      description: "Get expert feedback on your resume and improve your chances of landing an interview.",
-      href: "resume",
-   },
-   {
-      label: "Update Job Description",
-      description: "This should only be used for minor updates.",
-      href: "edit",
-   },
+  {
+    label: "Answer Technical Questions",
+    description: "Challenge yourself with practice questions tailored to your job description.",
+    href: "questions",
+  },
+  {
+    label: "Practice Interviewing",
+    description: "Simulate a real interview with AI-powered mock interviews.",
+    href: "interviews",
+  },
+  {
+    label: "Refine Your Resume",
+    description: "Get expert feedback on your resume and improve your chances of landing an interview.",
+    href: "resume",
+  },
+  {
+    label: "Update Job Description",
+    description: "This should only be used for minor updates.",
+    href: "edit",
+  },
 ];
 
 export default async function JobInfoPage({ params }: { params: Promise<{ jobInfoId: string }> }) {
-   const { jobInfoId } = await params;
+  const { jobInfoId } = await params;
 
-   const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
-   if (!uuidRegex.test(jobInfoId)) {
-      return notFound();
-   }
+  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+  if (!uuidRegex.test(jobInfoId)) {
+    return notFound();
+  }
 
-   const jobInfo = getCurrentUser().then(async ({ userId, redirectToSignIn }) => {
-      if (userId == null) return redirectToSignIn();
+  const jobInfo = getCurrentUser().then(async ({ userId, redirectToSignIn }) => {
+    if (userId == null) return redirectToSignIn();
 
-      const jobInfo = await getJobInfo(jobInfoId, userId);
-      if (jobInfo == null) return notFound();
+    const jobInfo = await getJobInfo(jobInfoId, userId);
+    if (jobInfo == null) return notFound();
 
-      return jobInfo;
-   });
+    return jobInfo;
+  });
 
-   return (
-      <div className="container my-4 space-y-4">
-         <BackLink href="/app">Dashboard</BackLink>
+  return (
+    <div className="container my-4 space-y-4">
+      <BackLink href="/app">Dashboard</BackLink>
 
-         <div className="space-y-6">
-            <header className="space-y-4">
-               <div className="space-y-2">
-                  <h1 className="text-3xl md:text-4xl">
-                     <SuspendedItem item={jobInfo} fallback={<Skeleton className="w-48" />} result={(j) => <>{j.name}</>} />
-                  </h1>
-                  <div className="flex gap-2">
-                     <SuspendedItem
-                        item={jobInfo}
-                        fallback={<Skeleton className="w-12" />}
-                        result={(j) => <Badge variant="secondary">{formatExperienceLevel(j.experienceLevel)}</Badge>}
-                     />
-                     <SuspendedItem item={jobInfo} fallback={null} result={(j) => j.title && <Badge variant="secondary">{j.title}</Badge>} />
-                  </div>
-               </div>
-               <p className="text-muted-foreground line-clamp-3">
-                  <SuspendedItem item={jobInfo} fallback={<Skeleton className="w-96" />} result={(j) => j.description} />
-               </p>
-            </header>
-
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 has-hover:*:not-hover:opacity-70">
-               {options.map((option) => (
-                  <Link
-                     className="hover:scale-[1.02] transition-[transform_opacity]"
-                     key={option.href}
-                     href={`/app/job-infos/${jobInfoId}/${option.href}`}
-                  >
-                     <Card className="h-full flex items-start justify-between flex-row">
-                        <CardHeader className="flex-grow">
-                           <CardTitle>{option.label}</CardTitle>
-                           <CardDescription>{option.description}</CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                           <ArrowRightIcon className="size-6" />
-                        </CardContent>
-                     </Card>
-                  </Link>
-               ))}
+      <div className="space-y-6">
+        <header className="space-y-4">
+          <div className="space-y-2">
+            <h1 className="text-3xl md:text-4xl">
+              <SuspendedItem item={jobInfo} fallback={<Skeleton className="w-48" />} result={(j) => <>{j.name}</>} />
+            </h1>
+            <div className="flex gap-2">
+              <SuspendedItem
+                item={jobInfo}
+                fallback={<Skeleton className="w-12" />}
+                result={(j) => <Badge variant="secondary">{formatExperienceLevel(j.experienceLevel)}</Badge>}
+              />
+              <SuspendedItem item={jobInfo} fallback={null} result={(j) => j.title && <Badge variant="secondary">{j.title}</Badge>} />
             </div>
-         </div>
+          </div>
+          <p className="text-muted-foreground line-clamp-3">
+            <SuspendedItem item={jobInfo} fallback={<Skeleton className="w-96" />} result={(j) => j.description} />
+          </p>
+        </header>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 has-hover:*:not-hover:opacity-70">
+          {options.map((option) => (
+            <Link className="hover:scale-[1.02] transition-[transform_opacity]" key={option.href} href={`/app/job-infos/${jobInfoId}/${option.href}`}>
+              <Card className="h-full flex items-start justify-between flex-row">
+                <CardHeader className="flex-grow">
+                  <CardTitle>{option.label}</CardTitle>
+                  <CardDescription>{option.description}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <ArrowRightIcon className="size-6" />
+                </CardContent>
+              </Card>
+            </Link>
+          ))}
+        </div>
       </div>
-   );
+    </div>
+  );
 }
 
 async function getJobInfo(jobInfoId: string, userId: string) {
-   "use cache";
-   cacheTag(getJobInfoIdTag(jobInfoId));
+  "use cache";
+  cacheTag(getJobInfoIdTag(jobInfoId));
 
-   return db.query.JobInfoTable.findFirst({
-      where: and(eq(JobInfoTable.id, jobInfoId), eq(JobInfoTable.userId, userId)),
-   });
+  return db.query.JobInfoTable.findFirst({
+    where: and(eq(JobInfoTable.id, jobInfoId), eq(JobInfoTable.userId, userId)),
+  });
 }
